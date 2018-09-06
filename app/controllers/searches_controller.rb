fab01a4d6b1badee46351788fe535aa4be9cdf39
Monica Tamaru Jones <mntamaru@gmail.com>
2018-09-06 14:05:33 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -31,6 +31,8 @@ class SearchesController < ApplicationController
   
   def friends
     resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
-      
+      req.params['oauth_token'] = session[:token]
+      # don't forget that pesky v param for versioning
+      req.params['v'] = '20160201'
   end
 end
