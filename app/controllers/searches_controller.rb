7dcb0f9b013a8c03d39e369012ff2e2e91d8724c
Monica Tamaru Jones <mntamaru@gmail.com>
2018-09-06 14:06:14 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -36,6 +36,6 @@ class SearchesController < ApplicationController
       req.params['v'] = '20160201'
     end
     
-    @friends = JSON.parse(resp.body)
+    @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
   end
 end
