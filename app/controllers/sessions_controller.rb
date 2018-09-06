3b3ea3d8b2af1ef6a8c02710314ee9c6a60ab35b
Monica Tamaru Jones <mntamaru@gmail.com>
2018-09-06 14:00:48 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,6 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
   
   def create
-    resp = Faraday.get()
+    resp = Faraday.get("https://foursquare.com/oauth2/access_token")
   end
 end
